9079642e256a0085c5d1809455137e9a88e5c36a
jinjun <757258777@qq.com>
2022-06-08 15:03:18 +0800

    [fix]:调整命名空间信息

--- src/Kernel/AccessToken.php
+++ src/Kernel/AccessToken.php
@@ -4,7 +4,6 @@ namespace Cblink\Service\LowCodeSdk\Kernel;
 
 class AccessToken extends \Cblink\Service\Foundation\AccessToken
 {
-
     public function getBaseUrl()
     {
         return $this->app->config['idaas_url'];
